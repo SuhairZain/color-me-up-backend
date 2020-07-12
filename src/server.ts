200917644a5ff63bc144090fe4865ff9a5d41f7c
Suhair Zain <suhair@languagezen.com>
2020-07-12 20:15:11 +0530

    Create /api/start endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,43 @@
 import express from "express";
 
+import { createBoard } from "color-me-up-shared";
+
+const BASE_URL = "/api";
+
 export const createServer = (port: number) => {
     const app = express();
+    app.use(express.json());
 
     const NODE_ENV = app.get("env");
 
-    app.get("/", (_, res) => res.send("Hello World!"));
+    app.get(`${BASE_URL}/start`, (req, res) => {
+        const { size = "6", numberOfColors = "6" } = req.query;
+
+        const sizeInt = Number.parseInt(typeof size === "string" ? size : "");
+        const numberOfColorsInt = Number.parseInt(
+            typeof numberOfColors === "string" ? numberOfColors : ""
+        );
+
+        if (sizeInt <= 0 || numberOfColorsInt <= 0) {
+            res.status(400).json({
+                reason: "Invalid params",
+                params: { size, numberOfColors },
+            });
+            return;
+        }
+
+        const board = createBoard(sizeInt, numberOfColorsInt);
+
+        res.json(board);
+    });
 
     // If we listen on test env and we test in watch mode, an error
     // will be thrown because of the existing listener not being cleared.
     if (NODE_ENV !== "test") {
         app.listen(port, () =>
-            console.log(`Example app listening at http://localhost:${port}`)
+            console.log(
+                `Color me up backend listening at http://localhost:${port}`
+            )
         );
     }
 
--- tests/server.test.ts
+++ tests/server.test.ts
@@ -2,9 +2,11 @@ import http from "http";
 
 import request from "supertest";
 
+import { Board } from "color-me-up-shared";
+
 import { createServer } from "../src/server";
 
-describe("GET /", () => {
+describe("Color me up backend", () => {
     let app: ReturnType<typeof createServer>;
     let server: http.Server;
 
@@ -18,10 +20,50 @@ describe("GET /", () => {
         server.close(done);
     });
 
-    it("returns 200 OK and the message 'Hello World!'", async () => {
-        const response = await request(app).get("/");
+    describe("GET /start", () => {
+        it("Creates a board with default options", async () => {
+            const response = await request(app).get("/api/start");
 
-        expect(response.status).toBe(200);
-        expect(response.text).toBe("Hello World!");
+            expect(response.status).toBe(200);
+
+            const board: Board = response.body;
+
+            expect(Array.isArray(board.colors)).toBe(true);
+            expect(board.colors.length).toBeGreaterThan(0);
+
+            expect(Array.isArray(board.tiles)).toBe(true);
+            expect(board.colors.length).toBe(6);
+
+            expect(board.tiles.length).toBe(6);
+            expect(board.tiles[0].length).toBe(6);
+        });
+
+        it("Creates a board of the required size", async () => {
+            const response = await request(app)
+                .get("/api/start")
+                .query({ size: 3, numberOfColors: 3 });
+
+            expect(response.status).toBe(200);
+
+            const board: Board = response.body;
+
+            expect(board.colors.length).toBe(3);
+
+            expect(board.tiles.length).toBe(3);
+            expect(board.tiles[0].length).toBe(3);
+        });
+
+        it("Returns 400 when params are invalid", async () => {
+            const response = await request(app)
+                .get("/api/start")
+                .query({ size: "-12", numberOfColors: false });
+
+            expect(response.status).toBe(400);
+
+            expect(response.body).toEqual({
+                reason: "Invalid params",
+                params: { size: "-12", numberOfColors: "false" },
+            });
+        });
     });
 });
